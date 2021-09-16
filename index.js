61fd860196eede895bef7de85128ac8421efa295
aareteasharma <90687658+aareteasharma@users.noreply.github.com>
2021-09-16 11:12:04 +0530

    Updated Version 1.0.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('CICD PoC App v1.0.0!')
 })
 
 app.get('/status', (req, res) => {
